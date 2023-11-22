905bc19ef52a35ca9f5f49e40e6155c97327c72d
PedroPenhaa <pedro.eng98@gmail.com>
2023-11-22 13:31:09 -0300

    as

--- app/Console/Commands/Skus.php
+++ app/Console/Commands/Skus.php
@@ -4,7 +4,9 @@ namespace App\Console\Commands;
 
 use App\Services\AuthLett;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
+use App\Models\Sku;
+use App\Models\Brick;
+use App\Models\Brand;
 
 class Skus extends Command
 {
@@ -27,7 +29,49 @@ class Skus extends Command
      */
     public function handle()
     {
-        $data = AuthLett::getData('skus', 2);
-        dd($data);
+        // Método Principal
+        // Recupera os registros da tabela Family, reduzindo em um array associativo usando o 'externel_id' como chave;
+        $bricks = Brick::get()->reduce(function ($acc, $brick) {
+            $acc[$brick->external_id] = $brick;
+            return $acc;
+        });
+
+        $brands = Brand::get()->reduce(function ($acc, $brand) {
+            $acc[$brand->external_id] = $brand;
+            return $acc;
+        });
+
+
+
+        $currentPage = 1;
+
+        do {
+            // Obter dados do serviço AuthLett para a página atual
+            $data = AuthLett::getData('skus', 2, $currentPage);
+            $decodedData = json_decode($data, true);
+            $pages = $decodedData['paging']['number_of_pages'];
+
+            // Iterando sobre os dados recebidos
+            foreach ($decodedData['data'] as $segmentData) {
+
+                //Atualiza ou Cria um registro na tabela.      
+                Sku::updateOrCreate(
+                    // Primeiro Array que será para validação.
+                    [
+                        'external_id' => $segmentData['id'],
+                        'brick_id' => $bricks[$segmentData['brick_id']]->id,
+                        'brand_id' => $brands[$segmentData['brand_id']]->id
+                    ],
+                    // Array que pode ser alterado os dados
+                    [
+                        'ean' => $segmentData['ean'],
+                        'retailer_sku_match' => $segmentData['retailer_sku_match'],
+                        'content' => $segmentData['content'],
+                    ]
+                );
+            }
+            // Incrementa a página atual para obter os dados da próxima página
+            $currentPage++;
+        } while ($currentPage <= $pages);
     }
 }
--- database/migrations/2023_11_20_185404_skus.php
+++ database/migrations/2023_11_20_185404_skus.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('skus', function (Blueprint $table) {
             $table->id();
             $table->string('ean');
+            $table->integer('external_id');
+
             $table->json('retailer_sku_match');
             $table->json('content');
 
@@ -21,7 +23,6 @@ return new class extends Migration
 
             $table->unsignedBigInteger('brick_id');
             $table->foreign('brick_id')->references('id')->on('bricks');
-
 
             $table->unsignedBigInteger('brand_id');
             $table->foreign('brand_id')->references('id')->on('brands');
