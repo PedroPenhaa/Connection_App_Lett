66617baa176c8fa42e700721e5a0cf54cae358fe
PedroPenhaa <pedro.eng98@gmail.com>
2023-11-23 13:34:52 -0300

    Bricks_Updating

--- app/Console/Commands/Bricks.php
+++ app/Console/Commands/Bricks.php
@@ -28,45 +28,37 @@ class Bricks extends Command
      */
     public function handle()
     {
-        $foreignKey = AuthLett::getForeignkey('App\Models\Classe');
         $currentPage = 1;
+        $perPage = 100;
 
-        $data = AuthLett::getData('bricks', 100, $currentPage);
-        $decodedData = json_decode($data, true);
-        $pages = $decodedData['paging']['number_of_pages'];
+        $foreignKey = AuthLett::getForeignkey('App\Models\Classe');
+        $totalPages = AuthLett::getTotalPages('bricks', $perPage);
 
-        /* Adiciona barra de progreso */
-        $bar = $this->output->createProgressBar($pages);
+        $bar = $this->output->createProgressBar($totalPages);
 
         do {
 
-            $data = AuthLett::getData('bricks', 100, $currentPage);
+            $data = AuthLett::getData('bricks', $perPage, $currentPage);
             $decodedData = json_decode($data, true);
             $pages = $decodedData['paging']['number_of_pages'];
 
-            /*Inicia a transação de dados*/
             DB::beginTransaction();
-
 
             foreach ($decodedData['data'] as $segmentData) {
 
-                /* Adiciona os dados na tabela */
                 Brick::updateOrCreate(
-                    /* Condição de comparação. Se tiver esses atributos ele atualiza, caso não, cria. */
                     [
                         'external_id' => $segmentData['id'],
                         'class_id' => $foreignKey[$segmentData['class_id']]->id
                     ],
-                    /* Altera os atributos */
                     [
                         'name' => $segmentData['name'],
                     ]
                 );
             }
+
             DB::commit();
-
             $currentPage++;
-
             $bar->advance();
         } while ($currentPage <= $pages);
     }
